b631ad0041b81e473157b65de2378f82fee58414
UGREEN USER <n.aliaga@proton.me>
2026-08-07 11:56:04 +0200

    1.1.6

--- utils.py
+++ utils.py
@@ -210,7 +210,7 @@ def get_file_lock(filepath):
 # ==========================================
 # 🔧 Footer global
 # ==========================================
-BOT_VERSION = "GGE Assistant • Version 1.1.5"
+BOT_VERSION = "GGE Assistant • Version 1.1.6"
 
 async def setup_embed_footer(embed: discord.Embed, interaction: discord.Interaction = None, langue: str = "fr", custom_server: str = None):
     txt = BOT_VERSION
