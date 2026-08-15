c83f205fbbb359d2659c327e35584ce1392c6c3b
UGREEN USER <n.aliaga@proton.me>
2026-08-15 18:26:01 +0200

    V2

--- cogs/storms.py
+++ cogs/storms.py
@@ -41,7 +41,7 @@ async def save_storm_config(data):
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 # ==========================================
-# ⚙️ CONFIGURATION DES ID DE L'API STORM
+# ⚙️ CONFIGURATION DES ID DE L'API STORMS
 # ==========================================
 FORT_LEVELS_MAPPING = {
     10: {"lvl": 40, "desc": "40"},
